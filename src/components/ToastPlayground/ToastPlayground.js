56a2d2fd312293e0efa35b2e192b62ae0d001844
Michael Schlueter <schluet0r4life@gmail.com>
2023-04-26 19:18:35 +0200

    add conditional rendering and dismiss functionality

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Button from "../Button";
+import Toast from "../Toast";
 
 import styles from "./ToastPlayground.module.css";
 
@@ -8,7 +9,12 @@ const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
   const [message, setMessage] = React.useState("");
-  const [variant, setVariant] = React.useState("");
+  const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [isRendered, setIsRendered] = React.useState(false);
+
+  function handleDismiss() {
+    setIsRendered(false);
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -16,6 +22,12 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      {isRendered && (
+        <Toast variant={variant} handleDismiss={handleDismiss}>
+          {message}
+        </Toast>
+      )}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -65,7 +77,11 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button onClick={() => {
+              setIsRendered(true);
+            }}>
+              Pop Toast!
+            </Button>
           </div>
         </div>
       </div>
